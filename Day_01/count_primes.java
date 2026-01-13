06573863281c0c84d860394616037b43c25e5157
Anant-27 <anantseth2709@gmail.com>
2026-01-13 15:05:02 +0530

    "Change file name"

--- Day_01/count_primes.java
+++ Day_01/count_primes.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class three_sum {
+public class count_primes {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
